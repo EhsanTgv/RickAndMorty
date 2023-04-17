5b122e20338350739c6d7deac1a2daf2cac79724
EhsanTgv <EhsanTgv@gmail.com>
2023-04-17 14:36:26 +0300

    Implement Data Models

---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Ehsan Taghavi on 17.04.2023.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
