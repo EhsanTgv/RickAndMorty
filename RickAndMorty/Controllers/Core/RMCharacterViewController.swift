2bcdf68dd7735cbf75daeae7c43d5bb5362c99c9
EhsanTgv <EhsanTgv@gmail.com>
2023-04-28 14:09:50 +0300

    Implement API Request

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -14,6 +14,14 @@ final class RMCharacterViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        print("here")
+        
+        let request = RMRequest(
+            endpoint: .character
+        )
+        
+        print(request.url)
     }
 
 }
