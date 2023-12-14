be2102b93d2e795848f5b4c21516bccd3907c301
EhsanTgv <EhsanTgv@gmail.com>
2023-12-14 11:55:11 +0300

    Implement Character Cell

---
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacterCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Ehsan Taghavi on 14.12.2023.
+//
+
+import Foundation
---
+++ RickAndMorty/Views/RMCharacterCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RMCharacterCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by Ehsan Taghavi on 14.12.2023.
+//
+
+import UIKit
+
+class RMCharacterCollectionViewCell: UICollectionViewCell {
+    
+}
